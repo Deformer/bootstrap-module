e568fd9dc3eb2c94149e95e6eba624b01f364016
Сергей Белан <serg19-03@mail.ru>
2018-12-09 00:34:35 +0300

    refactoring test

--- test/index.test.js
+++ test/index.test.js
@@ -4,12 +4,18 @@ const axios = require('axios');
 const { isString, isNumber, get, head, last } = require('lodash');
 require('jest-extended');
 
-let socket;
 let authRoomSocket;
+const testUser = {
+  login: 'serg1',
+  name: 'Sergey Belan',
+  password: '123456',
+  id: 1
+};
+const testMessage = 'Hello World';
+
 let agent;
 let room;
 
-const apiServerUrl = 'http://localhost:3000';
 
 const isRoom = (room) => isNumber(room.id) && isString(room.title);
 const isMessage = (message) => isString(message.text)
@@ -30,7 +36,7 @@ beforeAll((done) => {
   });
 
   agent = axios.create({
-    baseURL: apiServerUrl,
+    baseURL: 'http://localhost:3000',
     timeout: 200,
     httpAgent: new http.Agent({ keepAlive: true }),
     withCredentials: true
@@ -50,7 +56,7 @@ afterAll((done) => {
 
 describe('test case #1', () => {
   test('should communicate', (done) => {
-    socket = io.connect(`http://[localhost]:3000`, {
+    const socket = io.connect(`http://[localhost]:3000`, {
       'reconnection delay': 0,
       'reopen delay': 0,
       'force new connection': true,
@@ -72,18 +78,18 @@ describe('test case #1', () => {
 
         const user = response.data;
         expect(user).toBeObject();
-        expect(user.login).toBe('serg1');
-        expect(user.name).toBe('Sergey Belan');
+        expect(user.login).toBe(testUser.login);
+        expect(user.name).toBe(testUser.name);
         expect(data).toBeObject();
-        expect(data.message).toBe('User Sergey Belan logged in');
+        expect(data.message).toBe(`User ${testUser.name} logged in`);
 
 
         done();
       });
 
       response = await agent.post(`api/users/login`, {
-        login: 'serg1',
-        password: '123456'
+        login: testUser.login,
+        password: testUser.password
       });
     } catch(err) {
       done(err)
@@ -137,8 +143,8 @@ describe('test case #1', () => {
       let start;
       roomSocket.on('new message', (data) => {
         expect(data).toBeObject();
-        expect(data.message).toBe('Hello World!');
-        expect(data.username).toBe('Sergey Belan');
+        expect(data.message).toBe(testMessage);
+        expect(data.username).toBe(testUser.name);
         const delay = Date.now() - start;
         expect(delay < 200).toBeTrue();
 
@@ -148,14 +154,14 @@ describe('test case #1', () => {
       try {
         start = Date.now();
         response = await agent.post('api/messages', {
-          text: "Hello World!",
+          text: testMessage,
           roomId: room.id
         });
       } catch (err) {
         done(err);
       }
     });
-  })
+  });
 
   test('should have new message', async (done) => {
     const response = await agent.get(`api/rooms/${room.id}/messages`);
@@ -167,8 +173,8 @@ describe('test case #1', () => {
     const newMessage = last(messages);
     expect(newMessage).toBeObject();
     expect(isMessage(newMessage));
-    expect(newMessage.text === 'Hello World!').toBeTrue();
-    expect(newMessage.user.name === 'Sergey Belan').toBeTrue();
+    expect(newMessage.text === testMessage).toBeTrue();
+    expect(newMessage.user.name === testUser.name).toBeTrue();
     done();
   })
 
