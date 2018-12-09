0d9688b5b60adb8256c50b54edf1d98a91f4e4e4
Сергей Белан <serg19-03@mail.ru>
2018-12-09 14:27:21 +0300

    add docker-compose for testing

---
+++ get-repos.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Deformer/chat-service.git repos/chat-service
+git clone https://github.com/Deformer/database-module.git repos/database-module
+git clone https://github.com/Deformer/auth-service.git repos/auth-service
+git clone https://github.com/Deformer/web-sockets.git repos/web-sockets
+echo "DONE"
